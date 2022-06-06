003a934764539535a97e616f20affffa859cffe3
Joe <spizjo01@gmail.com>
2022-06-06 17:28:39 -0400

    commit tree 7f9ea052606dddc3bca5aaccb0f41a4abf9693ac

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(travelLength){
+  if (travelLength < 400) {
+    return "This one is on me!"
+  }
+  else if (travelLength <=2000) {
+    return "That will be twenty bucks."
+  }
+  else if (travelLength < 2500) {
+    return "I will gladly take your thirty bucks."
+  }
+  else if (travelLength > 2500) {
+    return "No can do."
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(name){
+  return (name === "NYC" ? "Ok, sounds good." : "No go.")
   // Write your code here!
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous" : return "Thank you so much.";
+    break;
+    case "not as generous" : return "Thank you.";
+    break;
+    default: return "Bye."
+    
+  }
   // Write your code here!
 }
